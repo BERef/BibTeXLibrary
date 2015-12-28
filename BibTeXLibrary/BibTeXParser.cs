eb9fa164b191c190995339b266c51a570570dea1
Blueve <672454911@qq.com>
2015-12-28 17:39:48 +0800

    Init project
    1. Lib project
    2. Unit test project

---
+++ BibTeXLibrary/BibTeXParser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeXLibrary
+{
+    public class BibTeXParser : IDisposable
+    {
+        #region Private field
+        /// <summary>
+        /// Input text stream.
+        /// </summary>
+        private readonly TextReader _inputText;
+        #endregion
+
+        #region Constructor
+        public BibTeXParser(TextReader inputText)
+        {
+            _inputText = inputText;
+        }
+        #endregion
+
+        #region Impement interface "IDisposable"
+        /// <summary>
+        /// Dispose stream resource.
+        /// </summary>
+        public void Dispose()
+        {
+            _inputText.Dispose();
+        }
+        #endregion
+    }
+}
