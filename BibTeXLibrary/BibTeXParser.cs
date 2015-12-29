5fedbef1503143b97a16757b2a6fedc0c97670fe
Blueve <672454911@qq.com>
2015-12-29 23:07:19 +0800

    Implemented lexer

--- BibTeXLibrary/BibTeXParser.cs
+++ BibTeXLibrary/BibTeXParser.cs
@@ -9,17 +9,100 @@ namespace BibTeXLibrary
 {
     public class BibTeXParser : IDisposable
     {
+        #region Const field
+
+        #endregion
+
         #region Private field
         /// <summary>
         /// Input text stream.
         /// </summary>
         private readonly TextReader _inputText;
+        
         #endregion
 
         #region Constructor
         public BibTeXParser(TextReader inputText)
         {
             _inputText = inputText;
+        }
+        #endregion
+
+        #region Public method
+        public void GetResult()
+        {
+            Parser();
+        }
+        #endregion
+
+        #region Private method
+        private void Parser()
+        {
+            int code;
+            char c;
+
+            while ((code = _inputText.Peek()) != -1)
+            {
+                c = (char)code;
+
+                if (c == '@')
+                {
+                    new Token(TokenType.Start);
+                }
+                else if (char.IsLetterOrDigit(c))
+                {
+                    StringBuilder value = new StringBuilder();
+                    
+                    while(true)
+                    {
+                        c = (char)_inputText.Read();
+                        value.Append(c);
+
+                        if ((code = _inputText.Peek()) == -1) break;
+                        c = (char)code;
+
+                        if (!char.IsLetterOrDigit(c)) break;
+                    }
+                    new Token(TokenType.String, value.ToString());
+                }
+                else if (c == '"')
+                {
+                    _inputText.Read();
+                    
+                    StringBuilder value = new StringBuilder();
+                    
+                    while((code = _inputText.Peek()) != -1)
+                    {
+                        c = (char)_inputText.Read();
+                        if (c == '"') break;
+
+                        value.Append(c);
+                    }
+                    new Token(TokenType.String, value.ToString());
+                }
+                else if (c == '{')
+                {
+                    new Token(TokenType.LeftBrace);
+                }
+                else if (c == '}')
+                {
+                    new Token(TokenType.RightBrace);
+                }
+                else if (c == ',')
+                {
+                    new Token(TokenType.Comma);
+                }
+                else if (c == '#')
+                {
+                    new Token(TokenType.Concatenation);
+                }
+                else if (!char.IsWhiteSpace(c))
+                {
+                    //TODO: need throw an exception
+                }
+                // Move to next char
+                _inputText.Read();
+            }
         }
         #endregion
 
