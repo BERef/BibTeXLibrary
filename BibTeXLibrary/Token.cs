dff54fea07ff0703fedf4096f186f551c55ca029
Blueve <672454911@qq.com>
2015-12-29 23:06:27 +0800

    Added class - Token

---
+++ BibTeXLibrary/Token.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeXLibrary
+{
+    internal struct Token
+    {
+        public TokenType Type;
+        public string Value;
+
+        public Token(TokenType type, string value = "")
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+
+    enum TokenType
+    {
+        Start,
+        
+        String,
+        Number,
+
+        Quotation,
+
+        LeftBrace,
+        RightBrace,
+
+        Comma,
+
+        Concatenation
+    }
+}
