060d5baed3c6b32295e07f96e894013cf00468b9
Blueve <672454911@qq.com>
2015-12-31 11:42:02 +0800

    New test case

--- UnitTest/BibParserTest.cs
+++ UnitTest/BibParserTest.cs
@@ -10,10 +10,18 @@ namespace UnitTest
     public class BibParserTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestParserRegularBibEntry()
         {
             var parser = new BibParser(
-                new StringReader("@article{keyword, title = {\"0\"{123}456{789}}, year = \"2012\"}"));
+                new StringReader("@article{keyword, title = {\"0\"{123}456{789}}, year = 2012, address=\"PingLeYuan\"}"));
+            parser.GetResult();
+        }
+
+        [TestMethod]
+        public void TestParserString()
+        {
+            var parser = new BibParser(
+                new StringReader("@article{keyword, title = \"hello \\\"world\\\"\",}"));
             parser.GetResult();
         }
     }
