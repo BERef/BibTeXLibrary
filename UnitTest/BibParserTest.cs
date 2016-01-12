e28971433d93580bcd05f8890897499a635a0b87
Blueve <672454911@qq.com>
2016-01-12 13:56:20 +0800

    New test case

--- UnitTest/BibParserTest.cs
+++ UnitTest/BibParserTest.cs
@@ -14,15 +14,45 @@ namespace UnitTest
         {
             var parser = new BibParser(
                 new StringReader("@article{keyword, title = {\"0\"{123}456{789}}, year = 2012, address=\"PingLeYuan\"}"));
-            parser.GetResult();
+            var entry = parser.GetAllResult()[0];
+
+            Assert.AreEqual("Article"           , entry.Type);
+            Assert.AreEqual("\"0\"{123}456{789}", entry.Title);
+            Assert.AreEqual("2012"              , entry.Year);
+            Assert.AreEqual("PingLeYuan"        , entry.Address);
         }
 
         [TestMethod]
         public void TestParserString()
         {
             var parser = new BibParser(
-                new StringReader("@article{keyword, title = \"hello \\\"world\\\"\",}"));
-            parser.GetResult();
+                new StringReader("@article{keyword, title = \"hello \\\"world\\\"\", address=\"Ping\" # \"Le\" # \"Yuan\",}"));
+            var entry = parser.GetAllResult()[0];
+
+            Assert.AreEqual("Article", entry.Type);
+            Assert.AreEqual("hello \\\"world\\\"", entry.Title);
+            Assert.AreEqual("PingLeYuan", entry.Address);
+        }
+
+        [TestMethod]
+        public void TestParserWithoutKey()
+        {
+            var parser = new BibParser(
+                            new StringReader("@book{, title = {}}"));
+            var entry = parser.GetAllResult()[0];
+
+            Assert.AreEqual("Book", entry.Type);
+            Assert.AreEqual("", entry.Title);
+        }
+
+        [TestMethod]
+        public void TestParserWithoutKeyAndTags()
+        {
+            var parser = new BibParser(
+                            new StringReader("@book{}"));
+            var entry = parser.GetAllResult()[0];
+
+            Assert.AreEqual("Book", entry.Type);
         }
     }
 }
