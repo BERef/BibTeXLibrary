a17330804e6a22e44869b799ca17e3687ee0a3a5
Blueve <672454911@qq.com>
2016-01-13 11:48:46 +0800

    Update test case

--- UnitTest/UnexpectedTokenExceptionTest.cs
+++ UnitTest/UnexpectedTokenExceptionTest.cs
@@ -14,7 +14,7 @@ namespace UnitTest
             var e = new UnexpectedTokenException(1, 10, TokenType.EOF, TokenType.Comma, TokenType.RightBrace);
             Assert.AreEqual(1, e.LineNo);
             Assert.AreEqual(10, e.ColNo);
-            Assert.AreEqual("Unexpected token: EOF. Expected: Comma, RightBrace", e.Message);
+            Assert.AreEqual("Line 1, Col 10. Unexpected token: EOF. Expected: Comma, RightBrace", e.Message);
         }
     }
 }
