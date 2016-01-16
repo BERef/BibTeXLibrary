1d8cd944cd4180d60bea5c5f0e8262faa9de4cbe
Blueve <672454911@qq.com>
2016-01-16 21:41:04 +0800

    Add abstract exception class

---
+++ BibTeXLibrary/ParserErrorException.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibTeXLibrary
+{
+    public abstract class ParserErrorException : ApplicationException
+    {
+        #region Private Field
+        /// <summary>
+        /// Customed error message.
+        /// </summary>
+        private readonly string _message;
+        #endregion
+
+        #region Public Field
+        /// <summary>
+        /// Line No.
+        /// </summary>
+        public readonly int LineNo;
+
+        /// <summary>
+        /// Col No.
+        /// </summary>
+        public readonly int ColNo;
+        #endregion
+
+        #region Constructor
+        public ParserErrorException(int lineNo, int colNo)
+        {
+            LineNo = lineNo;
+            ColNo  = colNo;
+        }
+        #endregion
+    }
+}
