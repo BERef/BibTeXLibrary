e6000214020d5a7825fdf083e3021b2c0632c814
Blueve <672454911@qq.com>
2016-01-18 15:33:43 +0800

    New test case

---
+++ UnitTest/UnrecognizableCharacterExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using BibTeXLibrary;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnrecognizableCharacterExceptionTest
+    {
+        [TestMethod]
+        public void TestConstructor()
+        {
+            var e = new UnrecognizableCharacterException(1, 10, '?');
+            Assert.AreEqual(1,  e.LineNo);
+            Assert.AreEqual(10, e.ColNo);
+            Assert.AreEqual("Line 1, Col 10. Unrecognizable character: '?'.", e.Message);
+        }
+    }
+}
