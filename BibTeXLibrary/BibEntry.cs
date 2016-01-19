07887c7134c3ef08da7202187d353a3e4f3f2685
Blueve <672454911@qq.com>
2016-01-19 14:38:39 +0800

    Add getter & add Key's property

--- BibTeXLibrary/BibEntry.cs
+++ BibTeXLibrary/BibEntry.cs
@@ -18,7 +18,7 @@ namespace BibTeXLibrary
         private EntryType _type;
 
         /// <summary>
-        /// Store all tags.
+        /// Store all tags
         /// </summary>
         private Dictionary<string, string> _tags = new Dictionary<string, string>();
         #endregion
@@ -34,113 +34,138 @@ namespace BibTeXLibrary
         public string Address
         {
             get { return this["address"]; }
+            set { this["address"] = value; }
         }
 
         public string Annote
         {
             get { return this["annote"]; }
+            set { this["annote"] = value; }
         }
 
         public string Author
         {
             get { return this["author"]; }
+            set { this["author"] = value; }
         }
 
         public string Booktitle
         {
             get { return this["booktitle"]; }
+            set { this["booktitle"] = value; }
         }
 
         public string Chapter
         {
             get { return this["chapter"]; }
+            set { this["chapter"] = value; }
         }
 
         public string Crossref
         {
             get { return this["crossref"]; }
+            set { this["crossref"] = value; }
         }
 
         public string Edition
         {
             get { return this["edition"]; }
+            set { this["edition"] = value; }
         }
 
         public string Editor
         {
             get { return this["editor"]; }
+            set { this["editor"] = value; }
         }
 
         public string Howpublished
         {
             get { return this["howpublished"]; }
+            set { this["howpublished"] = value; }
         }
 
         public string Institution
         {
             get { return this["institution"]; }
+            set { this["institution"] = value; }
         }
 
         public string Journal
         {
             get { return this["journal"]; }
+            set { this["journal"] = value; }
         }
 
         public string Mouth
         {
             get { return this["mouth"]; }
+            set { this["mouth"] = value; }
         }
 
         public string Note
         {
             get { return this["note"]; }
+            set { this["note"] = value; }
         }
 
         public string Number
         {
             get { return this["number"]; }
+            set { this["number"] = value; }
         }
 
         public string Organization
         {
             get { return this["organization"]; }
+            set { this["organization"] = value; }
         }
 
         public string Pages
         {
             get { return this["pages"]; }
+            set { this["pages"] = value; }
         }
 
         public string Publisher
         {
             get { return this["publisher"]; }
+            set { this["publisher"] = value; }
         }
 
         public string School
         {
             get { return this["shcool"]; }
+            set { this["school"] = value; }
         }
 
         public string Series
         {
             get { return this["series"]; }
+            set { this["series"] = value; }
         }
 
         public string Title
         {
             get { return this["title"]; }
+            set { this["title"] = value; }
         }
 
         public string Volume
         {
             get { return this["volume"]; }
+            set { this["volume"] = value; }
         }
 
         public string Year
         {
             get { return this["year"]; }
+            set { this["year"] = value; }
         }
 
+        /// <summary>
+        /// Entry's type
+        /// </summary>
         public string Type
         {
             get
@@ -153,11 +178,10 @@ namespace BibTeXLibrary
             }
         }
 
-        public string Key
-        {
-            get { return this["key"]; }
-            set { this["Key"] = value; }
-        }
+        /// <summary>
+        /// Entry's key
+        /// </summary>
+        public string Key { get; set; }
         #endregion
 
         #region Public Method
