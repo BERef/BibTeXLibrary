7aaf67bdbd8b0f4c5c6a4641ecc9d94849d8d9ff
Blueve <672454911@qq.com>
2016-01-31 17:32:55 +0800

    Add default value for property

--- BibTeXLibrary/Config.cs
+++ BibTeXLibrary/Config.cs
@@ -9,25 +9,13 @@ namespace BibTeXLibrary
     public static class Config
     {
         #region BibEntry ToString Config
-        private static string _retract = "  ";
-        private static string _lineFeed = "\n";
 
-        private static bool _align = false;
+        public static string Retract { get; private set; } = "  ";
 
-        public static string Retract
-        {
-            get { return _retract; }
-        }
+        public static string LineFeed { get; private set; } = "\n";
 
-        public static string LineFeed
-        {
-            get { return _lineFeed; }
-        }
+        public static bool Align { get; private set; } = false;
 
-        public static bool Align
-        {
-            get { return _align; }
-        }
         #endregion
 
         #region Public Static Method
