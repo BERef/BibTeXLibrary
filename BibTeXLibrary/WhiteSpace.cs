5de5ef7c7a32e51b3c070b6470088fc15d8ccbdb
Ikosy <lendres@fifthrace.com>
2023-04-26 17:56:18 -0500

    Initial check in of settings for bibliography writing.

---
+++ BibTeXLibrary/WhiteSpace.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace BibTeXLibrary
+{
+	/// <summary>
+	/// The type of character to fill white space with.
+	/// 
+	/// The "Description" attribute can be accessed using Reflection to get a string representing the enumeration type.
+	/// 
+	/// The "Length" enumeration can be used in loops as a convenient way of terminating a loop that does not have to be changed if
+	/// the number of items in the enumeration changes.  The "Length" enumeration must be the last item.
+	/// for (int i = 0; i &lt; (int)EnumType.Length; i++) {...}
+	/// </summary>
+	public enum WhiteSpace
+	{
+		/// <summary>Use spaces for white space.</summary>
+		[Description("Space")]
+		Space,
+
+		/// <summary>Use tabs for white space.</summary>
+		[Description("Tab")]
+		Tab,
+
+		/// <summary>The number of types/items in the enumeration.</summary>
+		[Description("Length")]
+		Length
+
+	} // End enum.
+} // End namespace.
---
+++ BibTeXLibrary/WriteSettings.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BibTeXLibrary
+{
+	/// <summary>
+	/// Settings to use when writing a bib file.
+	/// </summary>
+	public class WriteSettings
+	{
+		#region Members
+
+		private WhiteSpace	_whiteSpace					= WhiteSpace.Tab;
+		private int			_spacesPerTab				= 4;
+		private bool		_alignAtEquals				= true;
+		private int			_alignAtColumn				= 20;
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public WriteSettings()
+		{
+		}
+
+		#endregion
+
+		#region Properties
+
+		/// <summary>
+		/// Type of white space character to use.
+		/// </summary>
+		public WhiteSpace WhiteSpace { get => _whiteSpace; set => _whiteSpace = value; }
+
+		/// <summary>
+		/// The number of spaces per tab to use.
+		/// </summary>
+		public int SpacesPerTab { get => _spacesPerTab; set => _spacesPerTab = value; }
+
+		/// <summary>
+		/// Specifies if the tag values should be aligned at the equal sign.
+		/// </summary>
+		public bool AlignAtEquals { get => _alignAtEquals; set => _alignAtEquals = value; }
+
+		/// <summary>
+		/// Specifies the number of columns before the equal sign when aligning at the equal sign.
+		/// </summary>
+		public int AlignAtColumn { get => _alignAtColumn; set => _alignAtColumn = value; }
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region XML
+
+		#endregion
+
+	} // End class.
+} // End namespace.
