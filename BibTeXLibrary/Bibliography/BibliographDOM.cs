b0bddfbf36f456413fabf8bbceba29c16fbec9c0
Ikosy <lendres@fifthrace.com>
2023-05-12 19:37:50 -0500

    Add Bibliography Document Object Model

---
+++ BibTeXLibrary/Bibliography/BibliographDOM.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace BibTeXLibrary
+{
+	/// <summary>
+	/// Bibliography Document Object Model.
+	/// </summary>
+	public class BibliographDOM
+	{
+		#region Fields
+
+		#endregion
+
+		#region Construction
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public BibliographDOM()
+		{
+		}
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Methods
+
+		#endregion
+
+		#region XML
+
+		#endregion
+
+	} // End class.
+} // End namespace.
